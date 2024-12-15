f58faf39efd42f423fb3efa54e75f50333acb56d
root <root@server186.serverha.net>
2024-12-15 19:09:13 +0000

    finalize

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,30 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const fs = require("fs")
+
 module.exports = defineConfig({
+  css: {
+    extract: true, // Extract CSS into separate files in production
+  },
   transpileDependencies: true,
+
+  devServer: {
+    https: {
+      key: fs.readFileSync('./ssl/rioku_origin.key'),  // Path to your private key
+      cert: fs.readFileSync('./ssl/rioku_origin.crt'), // Path to your certificate
+    },
+    client: {
+      logging: 'none', // Suppresses any client-side logging
+      overlay: false,  // Disables the full-screen overlay on errors
+    },
+    host: "0.0.0.0",
+    port: 8080,
+    allowedHosts: 'all'
+  },
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
+      // Vuetify plugin options can go here
+    },
   },
-  devserver:{
-    host: "0.0.0.0",
-    port: 8080
-  }
-})
+});
+
